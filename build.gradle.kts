a642da2d1b415d755c5a3dd2835a3f21a6dea07e
Goooler <wangzongler@gmail.com>
2023-07-31 18:53:52 +0800

    Use pluginsUnderTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     testImplementation("org.spockframework:spock-core:$spockVersion") {
         exclude(mapOf("group" to "org.codehaus.groovy"))
     }
-    //"pluginsUnderTest"("gradle.plugin.com.github.johnrengelman:shadow:$shadowVersion")
+    pluginsUnderTest("gradle.plugin.com.github.johnrengelman:shadow:$shadowVersion")
 
     testImplementation("org.openjdk.jmh:jmh-core:$jmhVersion")
     testImplementation("org.openjdk.jmh:jmh-generator-bytecode:$jmhVersion")
