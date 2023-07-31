db37cf3c594a5c543cea603e272a6d5e84cbbc70
Goooler <wangzongler@gmail.com>
2023-07-31 19:55:33 +0800

    Remove outdated fields in BuildScanExtension

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,9 +29,6 @@ plugins {
 
 gradleEnterprise {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-
         publishAlways()
     }
 }
