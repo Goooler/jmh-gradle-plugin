06b6a74f6b87c219f9eff188f47ca9cc4d04dc8f
Goooler <wangzongler@gmail.com>
2024-08-25 16:10:22 +0800

    Remove withoutConfigurationCache for Kotlin

--- src/funcTest/groovy/me/champeau/jmh/MultiLanguageSpec.groovy
+++ src/funcTest/groovy/me/champeau/jmh/MultiLanguageSpec.groovy
@@ -28,7 +28,6 @@ class MultiLanguageSpec extends AbstractFuncSpec {
         given:
         usingSample("${language.toLowerCase()}-project")
         usingGradleVersion(gradleVersion)
-        if (language == 'Kotlin') withoutConfigurationCache('kotlin plugin unsupported')
 
         when:
         def result = build("jmh")
